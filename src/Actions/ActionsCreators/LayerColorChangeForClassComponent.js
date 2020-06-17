9f18ca50850cc4ca560f0edac72af016eab19e44
Mohab <homab3@gmail.com>
2020-06-17 08:07:57 +0200

    commit

--- src/Actions/ActionsCreators/LayerColorChangeForClassComponent.js
+++
@@ -1,9 +0,0 @@
-import { FillColorChange } from "../types";
-import { useSelector } from "react-redux";
-export const colorChanger=()=>dispatch=>{
-    const color = useSelector(state=>state.LayerColors.fillColor); 
-    dispatch({
-        type:FillColorChange,
-        payload:color
-    })
-}
